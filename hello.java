4715e741f03b4a384f16f5689313794c389489d2
Durjoy <da-durjoy@outlook.com>
2021-07-30 00:17:56 +0600

    Simple class Added

--- hello.java
+++ hello.java
@@ -2,5 +2,13 @@ class hello{
 	public static void main(String...r)
 	{
 	System.out.println("Hello World");
+	X obj=new X();
+	obj.name();
+	}
+}
+class X{
+	public void name()
+	{
+	System.out.println("da-durjoy@outlook.com");
 	}
 }
