09f87fd332711b7592dba5d7243e98602ca84382
Durjoy <da-durjoy@outlook.com>
2021-07-30 00:00:11 +0600

    Change one method

--- hello.java
+++ hello.java
@@ -2,5 +2,10 @@ class hello{
 	public static void main(String...r)
 	{
 	System.out.println("Hello World");
+	System.out.println("Incremented Value: "+Increment(5));
+	}
+	private static int Increment(int i)
+	{
+	return i+1;	
 	}
 }
