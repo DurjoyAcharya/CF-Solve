d4aa6972ef129ded230ac7867b1aac9639c18268
Durjoy <da-durjoy@outlook.com>
2022-03-14 10:31:33 +0600

    1651A

---
+++ src/main/java/Codeforces/PlayOff.java
@@ -0,0 +1,23 @@
+package Codeforces;
+
+//t = int(input())
+//for i in range(t):
+//    n = int(input())
+//    print(2 ** n - 1)
+public class PlayOff {
+    private static final java.util.Scanner sc=new java.util.Scanner(System.in);
+    public void Solve()
+    {
+        int temp= sc.nextInt();
+        for (int i = 0; i < temp; i++) {
+            int n= sc.nextInt();
+            System.out.println((int)(Math.pow(2,n))-1);
+        }
+    }
+    public static void main(String...r)
+    {
+        new PlayOff().Solve();
+
+    }
+
+}
