629787fa867cea35da61aca88411912a4b2de73a
Durjoy <da-durjoy@outlook.com>
2022-03-26 23:57:04 +0600

    MAS Test

---
+++ src/test/java/MAXimal/MaximumAverageSegmentTest.java
@@ -0,0 +1,11 @@
+package MAXimal;
+
+
+class MaximumAverageSegmentTest {
+
+    @org.junit.jupiter.api.Test
+    void algorithm_1() {
+        //new MaximumAverageSegment().Algorithm_1(new int[]{1,2,-3,-4,5});
+        new MaximumAverageSegment().Algorithm_FUllVersion(new int[]{1,-2,-3,-4,5});
+    }
+}
