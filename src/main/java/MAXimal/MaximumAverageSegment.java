c195dd026a66caad4777945bcaacc245482f9d7f
Durjoy <da-durjoy@outlook.com>
2022-03-26 23:57:51 +0600

    Full Version

--- src/main/java/MAXimal/MaximumAverageSegment.java
+++ src/main/java/MAXimal/MaximumAverageSegment.java
@@ -17,4 +17,34 @@ public class MaximumAverageSegment {
         System.out.println("Sum: "+sum);
         System.out.println("Min_Sum: "+min_sum);
     }
+    public void Algorithm_FUllVersion(int[] arr)
+    {
+        int ans=arr[0];
+        int ans_1=0;
+        int ans_r=0;
+        int sum=0;
+        int min_sum=0;
+        int min_pos=-1;
+        for (int i = 0; i < arr.length; i++) {
+            sum+=arr[i];
+            int cur=sum-min_sum;
+            if(cur>ans)
+            {
+                ans=cur;
+                ans_1=min_pos+1;
+                ans_r=i;
+            }
+            if (sum<min_sum)
+            {
+                min_sum=sum;
+                min_pos=i;
+            }
+        }
+        System.out.println("Answer: "+ans);
+        System.out.println("Answer 1: "+ans_1);
+        System.out.println("Answer r: "+ans_r);
+        System.out.println("Minimum Position: "+min_pos);
+        System.out.println("Sum: "+sum);
+        System.out.println("Min Sum: "+min_sum);
+    }
 }
