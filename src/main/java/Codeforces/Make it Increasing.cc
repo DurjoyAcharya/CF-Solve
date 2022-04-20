d57c4432462c32b32a37c7a7beab9e940d2f83ef
Durjoy <da-durjoy@outlook.com>
2022-04-20 19:39:43 +0600

    1667/A

---
+++ src/main/java/Codeforces/Make it Increasing.cc
@@ -0,0 +1,94 @@
+//https://codeforces.com/problemset/problem/1667/A
+#include<bits/stdc++.h>
+using namespace std;
+#pragma GCC optimize("O3")
+// #pragma GCC optimize("Ofast")
+// #pragma GCC target("avx,avx2,fma")
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+typedef tree<int, null_type, less<int>, rb_tree_tag,
+        tree_order_statistics_node_update> indexed_set;
+#define ll                  long long
+#define ull                 unsigned long long
+#define PI                  3.1415926535897932384626
+#define MOD                 998244353 // 1000000007
+#define vi                  vector<int>
+#define vb                  vector<bool>
+#define vvi                 vector<vector<int> >
+#define vll                 vector<long long>
+#define pb                  push_back
+#define eb                  emplace_back
+#define mp(a, b)            make_pair(a, b)
+#define pii                 pair<int, int>
+#define vpii                vector<pair<int, int> >
+#define mk(arr, n, type)    type *arr = new type[n];
+#define FOR(i, a, b)        for (int(i) = (a); (i) < (b); ++(i))
+#define RFOR(i, a, b)       for (int(i) = (a)-1; (i) >= (b); --(i))
+#define FORALL(i, a)        for (auto& (i) : (a))
+#define printall(a)         for (auto& (i) : (a)) cout << i << ' '
+#define print(a)            cout << a << ' '
+#define rsort(a)            sort((a).rbegin(), (a).rend())
+#define w(x)                int x; cin >> x; while(x--)
+#define all(c)              (c).begin(), (c).end()
+#define sz(c)               (int)(c).size()
+#define pqmx                priority_queue<int>
+#define pqmn                priority_queue<int, vector<int>, greater<int> >
+#define max3(a, b, c)       max((a), max((b), (c)))
+#define min3(a, b, c)       min((a), min((b), (c)))
+#define mx_all(c)           *max_element((c).begin(), (c).end())
+#define mn_all(c)           *min_element((c).begin(), (c).end())
+#define lwr_b(c, a)         lower_bound((c).begin(), (c).end(), (a)) - ((c).begin())
+#define upr_b(c, a)         upper_bound((c).begin(), (c).end(), (a)) - ((c).begin())
+#define FIO                 ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+void Solve()
+{
+  ll ans, i, j, n, k, l, c;
+
+    cin>>n;
+
+    ll a[n], b[n];
+
+    for(i=0; i<n; i++)
+    {
+        cin>>a[i];
+    }
+
+
+
+    for(i=0; i<n; i++)
+    {
+        b[i]=0;
+
+        c=0;
+
+        for(j=i-1; j>=0; j--)
+        {
+            k=abs(b[j+1]);
+
+            l=k/a[j]+1;
+
+            b[j]=-l*a[j];
+            c=c+l;
+        }
+
+        for(j=i+1; j<n; j++)
+        {
+            k=abs(b[j-1]);
+
+            l=k/a[j]+1;
+
+            b[j]=l*a[j];
+            c=c+l;
+        }
+
+        ans=min(ans, c);
+    }
+
+    cout<<ans<<"\n";
+}
+int32_t main(int argc,char** argv)
+{
+    FIO;
+    Solve();
+    return 0;
+}
