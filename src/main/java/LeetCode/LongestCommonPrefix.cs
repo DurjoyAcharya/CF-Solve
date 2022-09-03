61066272a71460cc90da24a56ba5c660c506d53f
Durjoy <da-durjoy@outlook.com>
2022-09-04 01:15:36 +0600

    Leetcode problem

---
+++ src/main/java/LeetCode/LongestCommonPrefix.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//https://leetcode.com/problems/longest-common-prefix/submissions/
+namespace Practice
+{
+
+    internal class LongestCommonPrefix
+    {
+        public string LongestCommonPrefixs(string[] strs)
+        {
+            var a = strs.Length;
+            if (a == 0)
+                return null;
+            if(a==1)
+                return strs[0];
+            Array.Sort(strs);
+            var end = Math.Min(strs[0].Length, strs[a-1].Length);
+            var i = 0;
+            while (i < end && strs[0][i] == strs[a - 1][i])
+                i++;
+            string pre=strs[0].Substring(0,i);
+            return pre; ;
+        }
+    }
+}
